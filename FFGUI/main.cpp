074936ef2d4120464a8dd8ddd7444f4415b93071
Emily <emil202821@hotmail.com>
2016-11-13 03:18:07 +0100

    Basics

--- FFGUI/main.cpp
+++ FFGUI/main.cpp
@@ -1,5 +1,48 @@
+#include <ui.h>
 
-int main()
+// Windows only
+#include <Windows.h>
+
+// C++ includes
+#include <string>
+#include <iostream>
+
+// Namespace
+using namespace std;
+
+// libui
+uiWindow *mainWindow;
+
+// Vars
+
+// UI Elements
+
+// Functions
+static int onClosing(uiWindow *w, void *data);
+
+int main(int argc, char *argv[])
 {
-	// TODO
+	// Init libui
+	uiInitOptions initOptions;
+	memset(&initOptions, 0, sizeof(uiInitOptions));
+	uiInit(&initOptions);
+
+	// Main window
+	mainWindow = uiNewWindow("FFGUI - GUI for FFmpeg", 848, 480, false);
+	uiWindowSetMargined(mainWindow, 1);
+	uiWindowOnClosing(mainWindow, onClosing, NULL);
+
+	// Show UI
+	uiControlShow(uiControl(mainWindow));
+	uiMain();
+
+	return 0;
+}
+
+static int onClosing(uiWindow *w, void *data)
+{
+	uiControlDestroy(uiControl(mainWindow));
+	uiUninit();
+	uiQuit();
+	return 0;
 }
